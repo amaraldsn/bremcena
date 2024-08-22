01161541bcb38a770369d95355d4d3ec35683803
kuberpunk666 <anonuumneharra@gmail.com>
2024-08-22 15:49:00 -0300

    add novas coisas otimização

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./tailwindbrasilemcena/paginaprincipal/assets/input.css/**/*.{html,js,css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
